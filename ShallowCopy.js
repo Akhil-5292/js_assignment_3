473348c0c3ea49bd918a9d42aa76de202669794c
Akhil-5292 <aps.ca9@gmail.com>
2023-05-25 16:18:45 +0530

    ShallowCopy

---
+++ ShallowCopy.js
@@ -0,0 +1,19 @@
+let a={name: 'sachin',
+age:23}
+
+let b=a
+
+b.name='vikesh'
+
+console.log(a);
+console.log(b);
+
+let x=[2,3,5,[6]]
+let c=x
+c=c[3].push(8)
+
+console.log(x)
+console.log(c)
+
+
+
